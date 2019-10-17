8d78aae4ec8e9bf87dd39a424a7b44571fb273ab
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-10-17 23:52:59 +0300

    first angry_os build

---
+++ kernel.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#if defined(__linux__)
+#error "You are not using a cross-compiler"
+#endif
+
+#if !defined(__i386__)
+#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#endif
+
+void kernel_main(void) {
+
+}
