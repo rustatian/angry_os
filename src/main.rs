af2ed39136f78efa0837714a34f49a28d6af95a0
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-10-27 19:31:54 +0300

    Minimal freestanding lib in Rust

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+// panic handler
+use core::panic::PanicInfo;
+
+/// Called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
