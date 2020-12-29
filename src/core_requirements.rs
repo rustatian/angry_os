72f50c7b20ff7d86760b322b168bcd837af4b41f
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-29 11:39:48 +0300

    Add some comments on movsb implementation

--- src/core_requirements.rs
+++ src/core_requirements.rs
@@ -1,3 +1,17 @@
+/// https://software.intel.com/sites/default/files/managed/9e/bc/64-ia-32-architectures-optimization-manual.pdf
+/// https://stackoverflow.com/questions/43343231/enhanced-rep-movsb-for-memcpy
+/// libc `memcpy` implementation in Rust
+///
+/// #Parameters
+///
+/// * `dest` - Pointer to memory to copy to
+/// * `src`  - Pointer to memory to copy from
+/// * `n`    - Number of bytes to copy
+///
+/// #Returns
+///
+/// Pointer to `dest`
+///
 #[no_mangle]
 #[cfg(target_arch = "x86_64")]
 pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
@@ -26,8 +40,8 @@ pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
     let mut ii = 0;
 
     while ii < n {
-        let a = *s1.offset(ii as isize);
-        let b = *s2.offset(ii as isize);
+        let a = *s1.add(ii);
+        let b = *s2.add(ii);
         if a != b {
             return (a as i32).wrapping_sub(b as i32);
         }
