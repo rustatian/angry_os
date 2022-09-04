3ed3b8f303a1c1ff971cf40c153d3d1818a646f3
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-09-04 20:34:08 +0200

    return back rust version
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/*
+1. C calling convention: https://en.wikipedia.org/wiki/Calling_convention
+ */
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
+}
+
+/*
+The eh_personality language item marks a function that is used for implementing stack unwinding.
+
+1. https://en.wikipedia.org/wiki/Runtime_system
+2. Rust init: https://github.com/rust-lang/rust/blob/master/library/std/src/rt.rs
+3.
+*/
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
