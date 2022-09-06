5b313853a683493be6669933bc1c2f790d724d99
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-09-06 12:43:50 +0200

    more docs
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use core::panic::PanicInfo;
 
 /*
 1. C calling convention: https://en.wikipedia.org/wiki/Calling_convention
+2. https://en.wikipedia.org/wiki/Crt0
  */
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
